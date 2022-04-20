db741b2199e357b11e756f0262e5a033ac3d31d3
B96R7 <bennetrohwer.123@gmail.com>
2022-04-20 23:39:32 +0200

    morecode

---
+++ cc20.04.22.js
@@ -0,0 +1,11 @@
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values.
+function swapValues() {
+    return arguments[0].reverse();
+}
+
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+function array(arr){
+    return arr.split(",").slice(1,-1).join(" ") || null;
+  }
+
+
