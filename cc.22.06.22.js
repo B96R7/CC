9af9d93b79c781b11778d5b3467c44aa4aa7ce0e
B96R7 <bennetrohwer.123@gmail.com>
2022-06-22 16:35:53 +0200

    morecode

---
+++ cc.22.06.22.js
@@ -0,0 +1,22 @@
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+function isAnagram (test, original) {
+    return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("")
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
